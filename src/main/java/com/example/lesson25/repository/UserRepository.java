8ca213631d5758be9ebe0b36baa246768c20b339
asrytikov <asrytikov@gmail.com>
2023-10-15 20:30:50 +0400

    Lesson25

--- src/main/java/com/example/lesson25/repository/UserRepository.java
+++ src/main/java/com/example/lesson25/repository/UserRepository.java
@@ -1,2 +1,7 @@
-package com.example.lesson25.repository;public class UserRepository {
+package com.example.lesson25.repository;
+
+import com.example.lesson25.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
--- src/main/java/com/example/lesson25/services/UserService.java
+++ src/main/java/com/example/lesson25/services/UserService.java
@@ -1,2 +1,19 @@
-package com.example.lesson25.services;public interface UserService {
+package com.example.lesson25.services;
+
+import com.example.lesson25.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User createUser(User user);
+
+    User updateUser(User user);
+
+    User getUserById(int id);
+
+    List<User> getAllUsers();
+
+    void deleteUser(int id);
+
 }
